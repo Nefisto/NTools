04d73ba19ecf6212143a7d65e8285d0085cf0dec
Nefisto <joao.gavron@gmail.com>
2022-04-07 09:17:42 -0300

    Create a class to lazy approach for a Dont destroy on load monobehaviour singleton

---
+++ Singletons/PersistentLazySingletonMonoBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using NTools;
+using UnityEngine;
+
+/// <summary>
+/// Lazy approach to create a singleton in a Dont Destroy on Load
+/// </summary>
+public class PersistentLazySingletonMonoBehaviour<T> : LazyBehavior
+    where T : Component
+{
+    protected static T instance { get; set; }
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+                CreatePersistentObject();
+
+            return instance;
+        }
+    }
+
+    private static void CreatePersistentObject()
+    {
+        var go = new GameObject(typeof(T).ToString());
+        instance = go.AddComponent<T>();
+        DontDestroyOnLoad(go);
+    }
+}
