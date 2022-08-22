60c40a2bd89787b71d875e8dc76805d54fd91ef1
Nefisto <joao.gavron@gmail.com>
2022-08-22 08:53:10 -0300

    Initial version of EventHandler
    This contains, Register, Unregister and Raise
    
    Co-Authored-By: Joao.Gavron <joao.gavron@gmail.com>

---
+++ EventHandler/EventHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace NTools
+{
+    /// <summary>
+    ///     If observers change the table size while this is running things can get really bad
+    /// </summary>
+    public static partial class EventHandler
+    {
+        private static readonly Dictionary<string, List<InvokableActionBase>> globalEventTable =
+            new Dictionary<string, List<InvokableActionBase>>();
+
+        private static readonly Dictionary<object, Dictionary<string, List<InvokableActionBase>>> eventTable =
+            new Dictionary<object, Dictionary<string, List<InvokableActionBase>>>();
+
+        public static void CleanEvents()
+        {
+            globalEventTable.Clear();
+
+            foreach (var (_, dictionary) in eventTable)
+                dictionary.Clear();
+            eventTable.Clear();
+        }
+    }
+}
