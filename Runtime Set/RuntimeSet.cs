4ce56e2a6fb96388213c39009dcc126fe8cc1357
Nefisto <joao.gavron@gmail.com>
2022-09-18 10:36:23 -0300

    Changed RuntimeSet.cs to an IList<RuntimeItem>

--- Runtime Set/RuntimeSet.cs
+++ Runtime Set/RuntimeSet.cs
@@ -1,27 +1,55 @@
 // This idea came from Ryan Hipple talk: https://www.youtube.com/watch?v=raQ3iHhE_Kk&t=184s 
 
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace NTools
 {
     [CreateAssetMenu(fileName = "RuntimeSet", menuName = "NTools/RuntimeSet")]
-    public class RuntimeSet : ScriptableObject
+    public class RuntimeSet : ScriptableObject, IList<RuntimeItem>
     {
-        public List<RuntimeItem> items;
+        [SerializeField]
+        private List<RuntimeItem> items;
+        
+        public IEnumerator<RuntimeItem> GetEnumerator()
+            => items.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => ((IEnumerable)items).GetEnumerator();
+
+        public void Add (RuntimeItem item)
+            => items.Add(item);
+
+        public void Clear()
+            => items.Clear();
+
+        public bool Contains (RuntimeItem item)
+            => items.Contains(item);
+
+        public void CopyTo (RuntimeItem[] array, int arrayIndex)
+            => items.CopyTo(array, arrayIndex);
+
+        public bool Remove (RuntimeItem item)
+            => items.Remove(item);
 
         public int Count => items.Count;
 
-        public void Add (RuntimeItem thing)
-        {
-            if (!items.Contains(thing))
-                items.Add(thing);
-        }
+        public bool IsReadOnly => ((ICollection<RuntimeItem>)items).IsReadOnly;
 
-        public void Remove (RuntimeItem thing)
+        public int IndexOf (RuntimeItem item)
+            => items.IndexOf(item);
+
+        public void Insert (int index, RuntimeItem item)
+            => items.Insert(index, item);
+
+        public void RemoveAt (int index)
+            => items.RemoveAt(index);
+
+        public RuntimeItem this [int index]
         {
-            if (items.Contains(thing))
-                items.Remove(thing);
+            get => items[index];
+            set => items[index] = value;
         }
     }
 }
