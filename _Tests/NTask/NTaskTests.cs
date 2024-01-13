fdbf842883019d9d28face57c01bde638197df3a
Nefisto <joao.gavron@gmail.com>
2024-01-13 15:11:15 -0300

    test: Added tests for yield break and yield return taking 1 and 0 frames respectivaly

---
+++ _Tests/NTask/NTaskTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class NTaskTests
+{
+    [UnityTest]
+    public IEnumerator RoutineThatJustBreaksShouldFinishOnTheSameFrameThatItsStarted()
+    {
+        var endFrame = 0;
+        var task = new NTask(MyRoutine(), false);
+        task.OnFinished += _ => endFrame = Time.frameCount;
+
+        yield return null;
+        var initialFrame = Time.frameCount;
+        task.Start();
+        
+        yield return new WaitForSeconds(.5f);
+
+        Assert.AreEqual(initialFrame, endFrame);
+        
+        IEnumerator MyRoutine()
+        {
+            yield return RoutineThatBreaks();
+            yield return RoutineThatBreaks();
+            yield return RoutineThatBreaks();
+        }
+    }
+
+    [UnityTest]
+    public IEnumerator RoutineThatYieldOneFrameAndMultipleBreaksShouldTakeExactlyOneFrame()
+    {
+        var endFrame = 0;
+        var task = new NTask(MyRoutine(), false);
+        task.OnFinished += _ => endFrame = Time.frameCount;
+
+        yield return null;
+        var initialFrame = Time.frameCount;
+        task.Start();
+        
+        yield return new WaitForSeconds(.5f);
+
+        Assert.AreEqual(endFrame - initialFrame, 1);
+        
+        IEnumerator MyRoutine()
+        {
+            yield return RoutineThatBreaks();
+            yield return RoutineThatTakesOneFrame();
+            yield return RoutineThatBreaks();
+        }
+    }
+    
+    private IEnumerator RoutineThatBreaks()
+    {
+        yield break;
+    }
+
+    private IEnumerator RoutineThatTakesOneFrame()
+    {
+        yield return null;
+    }
+}
