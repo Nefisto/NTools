a66094b65edb7ed6ee592d93eaaa1aaa02787440
Nefisto <joao.gavron@gmail.com>
2024-01-31 10:55:37 -0300

    feat: Added [Serializable] to EntryPoint.cs

--- Entry point/EntryPoint.cs
+++ Entry point/EntryPoint.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
 
 namespace NTools
 {
+    [Serializable]
     public class EntryPoint
     {
         private readonly List<Func<IEntryPointContext, IEnumerator>> actions = new();
