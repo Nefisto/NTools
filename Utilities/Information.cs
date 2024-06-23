7fbaacc16a2ebb9f67719630687fece319fd0811
Nefisto <joao.gavron@gmail.com>
2024-06-23 18:25:04 -0300

    feat: Added Information.cs component

---
+++ Utilities/Information.cs
@@ -0,0 +1,14 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+// ReSharper disable NotAccessedField.Local
+
+/// <summary>
+/// Component used to express information about some game object on inspector
+/// </summary>
+public class Information : MonoBehaviour
+{
+    [Multiline(5)]
+    [HideLabel]
+    [SerializeField]
+    private string message;
+}
