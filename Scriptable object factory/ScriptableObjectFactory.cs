a0a4d0a18a380fb880ff1cf99ac072cc8a4e371c
Nefisto <joao.gavron@gmail.com>
2024-10-23 11:45:41 -0300

    feat: Added Scriptable Object Factory

---
+++ Scriptable object factory/ScriptableObjectFactory.cs
@@ -0,0 +1,18 @@
+﻿using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UnityEngine;
+
+/// <summary>
+/// Responsable to serialize plain class on inspector allow me to work directly with plain class on code but also
+///     use unity as an injector 
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class ScriptableObjectFactory<T> : SerializedScriptableObject
+{
+    [HideLabel]
+    [InlineProperty]
+    [SerializeField]
+    public T serializedField;
+
+    public virtual T GetInstance() => (T)SerializationUtility.CreateCopy(serializedField);
+}
