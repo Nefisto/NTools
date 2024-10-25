445b591fac2e0e612b427e448bb1d096e79db6e0
Nefisto <joao.gavron@gmail.com>
2024-10-25 14:30:19 -0300

    feat: added ScreenFading

--- Extensions/Extensions_Color.cs
+++ Extensions/Extensions_Color.cs
@@ -4,6 +4,12 @@ namespace NTools
 {
     public static partial class Extensions
     {
+        public static Color32 SetAlpha (this Color color, float targetAlpha)
+        {
+            color.a = targetAlpha;
+            return color;
+        }
+        
         public static bool CompareWithoutAlpha (this Color color, Color other)
             => Mathf.Abs(color.r - other.r) < 0.001f &&
                Mathf.Abs(color.g - other.g) < 0.001f &&
---
+++ Screen fading/ScreenFading.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using NTools;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenFading : MonoBehaviour
+{
+    [TitleGroup("References")]
+    [SerializeField]
+    private Image image;
+
+    [TitleGroup("Debug")]
+    [ReadOnly]
+    [ShowInInspector]
+    private bool isFaded;
+
+    public IEnumerator FadeOut (Settings settings = null)
+    {
+        if (isFaded)
+            yield break;
+
+        yield return Fade(settings ?? new Settings(), 1f, 0f);
+        image.raycastTarget = false;
+        isFaded = true;
+    }
+
+    public IEnumerator FadeIn (Settings settings = null)
+    {
+        if (!isFaded)
+            yield break;
+
+        image.raycastTarget = true;
+        yield return Fade(settings ?? new Settings(), 0f, 1f);
+        isFaded = false;
+    }
+
+    private IEnumerator Fade (Settings settings, float from, float to)
+    {
+        image.color = settings.color;
+        // TODO: Create SetAlpha on NTools
+        image.color = image.color.SetAlpha(from);
+
+        var timer = 0f;
+        while (timer < settings.duration)
+        {
+            var lastFrameDuration = Time.deltaTime;
+
+            var currentPercentage = Mathf.Clamp(timer / settings.duration, 0f, 1f);
+            var alphaPercentage = from > to ? 1 - currentPercentage : currentPercentage;
+
+            image.color = image.color.SetAlpha(alphaPercentage);
+
+            timer += lastFrameDuration;
+            yield return null;
+        }
+
+        image.color = image.color.SetAlpha(to);
+    }
+    
+    public class Settings
+    {
+        public Color32 color = Color.black;
+
+        /// <summary>
+        /// In seconds
+        /// </summary>
+        public float duration = .5f;
+    }
+}
