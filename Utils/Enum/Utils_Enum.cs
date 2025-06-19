52bc84130b752b8af4a57bad3360bde1394023e6
Nefisto <joao.gavron@gmail.com>
2025-06-19 18:04:23 -0300

    moved enum utils to correct namespace

---
+++ Utils/Enum/Utils_Enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NTools
+{
+    public static partial class Utils
+    {
+        /// <summary>
+        /// Get enums as strings, pretty useful when applying enums to dropdowns 
+        /// </summary>
+        /// <returns>The enum names in a list</returns>
+        public static List<string> GetEnumNames<T>() where T : Enum => Enum.GetNames(typeof(T)).ToList();
+    }
+}
