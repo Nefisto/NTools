f69b4bb776a581445d3016bb2bbe1ac8917b2be8
Nefisto <joao.gavron@gmail.com>
2026-07-08 10:46:20 -0300

    feat: Update BasicVariable

---
+++ Utils/Basic variable/BasicVariable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class BasicVariable<T> : EventArgs
+{
+    public T Value { get; set; }
+    private BasicVariable (T value) => Value = value;
+    
+    public static implicit operator T(BasicVariable<T> variable) => variable.Value;
+    public static implicit operator BasicVariable<T>(T value) => new(value);
+}
