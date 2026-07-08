b106318672c9d85683466c1f9ef22bd7c21b0ebf
Nefisto <joao.gavron@gmail.com>
2026-07-08 10:22:21 -0300

    feat: update fade screen

---
+++ Services/Interface/IFadeScreenService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public interface IFadeScreenService
+{
+    public UniTask FadeInAsync (Settings settings = null);
+    public UniTask FadeOutAsync (Settings settings = null);
+
+    [Serializable]
+    public class Settings
+    {
+        [field: SerializeField]
+        public Image FadeImage { get; set; }
+
+        [field: SerializeField]
+        public float FadeDuration { get; set; } = 1f;
+
+        [field: SerializeField]
+        public Color Color { get; set; } = Color.black;
+    }
+}
---
+++ Services/Null object/NullFadeScreenService.cs
@@ -0,0 +1,8 @@
+﻿using Cysharp.Threading.Tasks;
+
+public class NullFadeScreenService : IFadeScreenService
+{
+    public UniTask FadeInAsync (IFadeScreenService.Settings settings = null) => UniTask.CompletedTask;
+
+    public UniTask FadeOutAsync (IFadeScreenService.Settings settings = null) => UniTask.CompletedTask;
+}
